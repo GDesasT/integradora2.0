a59b05f8d8a4ac0be6e665b81ccfb18628d2dc55
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-07-11 12:06:45 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/menu', [HomeController::class, 'menu'])->name('menu');
-Route::get('/login', [HomeController::class, 'login'])->name('login');
+Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::get('/feedback', [HomeController::class, 'feedback'])->name('feedback');
